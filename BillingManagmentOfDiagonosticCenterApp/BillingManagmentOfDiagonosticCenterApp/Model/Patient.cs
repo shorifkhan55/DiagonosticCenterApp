2800b0b4fa9e0239849caeaa5585d59a9177adcb
Ratnadip Kuri <ratnadipk983@gmail.com>
2016-10-30 12:49:06 +0600

    Test model added

---
+++ BillingManagmentOfDiagonosticCenterApp/BillingManagmentOfDiagonosticCenterApp/Model/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingManagmentOfDiagonosticCenterApp.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
---
+++ BillingManagmentOfDiagonosticCenterApp/BillingManagmentOfDiagonosticCenterApp/Model/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingManagmentOfDiagonosticCenterApp.Model
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Fee { get; set; }
+        public int TypeId { get; set; }
+    }
+}
